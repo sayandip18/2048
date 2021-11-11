48d8b1cc257f6a6b373a684dfd9410a9a22f7445
Sayandip Halder <sayandiph4@gmail.com>
2021-11-11 21:47:25 +0530

    add addNumber()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import cloneDeep from "lodash.clonedeep";
 
 function App() {
   const [data, setData] = useState([
@@ -7,6 +8,45 @@ function App() {
     [0, 0, 0, 0],
     [0, 0, 0, 0],
   ]);
+
+  const initialize = () => {
+
+    let newGrid = cloneDeep(data);
+    console.log(newGrid);
+
+    addNumber(newGrid);
+    console.table(newGrid);
+    addNumber(newGrid);
+    console.table(newGrid);
+    setData(newGrid);
+  };
+
+
+  const addNumber = (newGrid) => {
+    let added = false;
+    let gridFull = false;
+    let attempts = 0;
+    while (!added) {
+      if (gridFull) {
+        break;
+      }
+
+      let rand1 = Math.floor(Math.random() * 4);
+      let rand2 = Math.floor(Math.random() * 4);
+      attempts++;
+      if (newGrid[rand1][rand2] === 0) {
+        newGrid[rand1][rand2] = Math.random() > 0.5 ? 2 : 4;
+        added = true;
+      }
+      
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
+
+
   return (
     <div style={{
       background: "#AD9D8F",
